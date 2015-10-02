8e2c938b818887f4331f3d5bb9087bda58a0f61e
tbrams <torben@brams.dk>
2015-10-02 12:24:17 +0200

    Draw nice red boxes on an off-white background

---
+++ app/src/main/java/dk/brams/draganddraw/Box.java
@@ -0,0 +1,25 @@
+package dk.brams.draganddraw;
+
+
+import android.graphics.PointF;
+
+public class Box {
+    private PointF mOrigin;
+    private PointF mCurrent;
+
+    public Box(PointF origin) {
+        mOrigin = mCurrent = origin;
+    }
+
+    public PointF getOrigin() {
+        return mOrigin;
+    }
+
+    public PointF getCurrent() {
+        return mCurrent;
+    }
+
+    public void setCurrent(PointF current) {
+        mCurrent = current;
+    }
+}
--- app/src/main/java/dk/brams/draganddraw/BoxDrawingView.java
+++ app/src/main/java/dk/brams/draganddraw/BoxDrawingView.java
@@ -1,10 +1,22 @@
 package dk.brams.draganddraw;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class BoxDrawingView extends View {
+    private static final String TAG = "BoxDrawingView";
+    private Box mCurrentBox;
+    private ArrayList<Box> mBoxes = new ArrayList<Box>();
+    private Paint mBoxPaint;
+    private Paint mBackgroundPaint;
 
     // Used when creating the view in code
     public BoxDrawingView(Context context) {
@@ -15,5 +27,62 @@ public class BoxDrawingView extends View {
     public BoxDrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
+        // Use a semitransparent red for boxes
+        mBoxPaint = new Paint();
+        mBoxPaint.setColor(0x22ff0000);
+
+        // And off-white for the background
+        mBackgroundPaint = new Paint();
+        mBackgroundPaint.setColor(0xfff8efe0);
+
+
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF curr = new PointF(event.getX(), event.getY());
+        Log.i(TAG, "Received event at ("+curr.x + ","+curr.y+")");
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.i(TAG, " ACTION DOWN");
+                // Reset drawing state
+                mCurrentBox = new Box(curr);
+                mBoxes.add(mCurrentBox);
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                Log.i(TAG," ACTION_MOVE");
+                if (mCurrentBox!=null) {
+                    mCurrentBox.setCurrent(curr);
+                    invalidate();
+                }
+                break;
+
+            case MotionEvent.ACTION_UP:
+                Log.i(TAG," ACTION_UP");
+                mCurrentBox=null;
+                break;
+
+            case MotionEvent.ACTION_CANCEL:
+                Log.i(TAG," ACTION_CANCEL");
+                mCurrentBox=null;
+                break;
+        }
+
+        return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPaint(mBackgroundPaint);
+
+        for (Box box: mBoxes) {
+            float left = Math.min(box.getOrigin().x, box.getCurrent().x);
+            float right = Math.max(box.getOrigin().x, box.getCurrent().x);
+            float top = Math.min(box.getOrigin().y, box.getCurrent().y);
+            float bottom = Math.max(box.getOrigin().y, box.getCurrent().y);
+
+            canvas.drawRect(left, top, right, bottom, mBoxPaint);
+        }
     }
 }
