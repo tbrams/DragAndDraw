24fb2960ac8e3c9d130982b4e75965b8e0816f9b
tbrams <torben@brams.dk>
2015-10-02 11:50:18 +0200

    Initial commit

---
+++ app/src/main/java/dk/brams/draganddraw/BoxDrawingView.java
@@ -0,0 +1,19 @@
+package dk.brams.draganddraw;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class BoxDrawingView extends View {
+
+    // Used when creating the view in code
+    public BoxDrawingView(Context context) {
+        this(context, null);
+    }
+
+    // Used when creating the view from XML
+    public BoxDrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+}
---
+++ app/src/main/java/dk/brams/draganddraw/DragAndDrawActivity.java
@@ -0,0 +1,12 @@
+package dk.brams.draganddraw;
+
+import android.support.v4.app.Fragment;
+
+public class DragAndDrawActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new DragAndDrawFragment();
+    }
+
+}
